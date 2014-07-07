c17a012e54b789a3f819be64d8144e0ba964cab2
chromebookbob <chromebookbob@gmail.com>
2014-07-07 21:44:25 +0100

    Changed gui

--- nebulaic_toaster.py
+++ nebulaic_toaster.py
@@ -1,37 +1,26 @@
 from kivy.app import App
 
 from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.scatter import Scatter
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.properties import ListProperty
 from kivy.properties import StringProperty
+from kivy.properties import NumericProperty
 from time import sleep
 
 
 import random
+
+
+
 class ScatterTextWidget(BoxLayout, Image):
-	somestring = 'I just want to say:'
-	textboxtext = StringProperty(somestring)
-	text_color = ListProperty([7, 20, 55, 1])
-	
-	def rancolor(self, *args):
-		color = [random.random() for i in xrange(3)] + [1]
-		self.text_color = color
-		
-	def printout(text, self, *args):
-		
-		self.somestring = firsttext + text
-		self.textboxtext = StringProperty(self.somestring)
-		
-			
-	def button1press():
-		printout("Hello World!")
-		
-class StartupGif(BoxLayout, Image):
+	pass
+class StartupGif(BoxLayout):
+	windowidth = NumericProperty(1000)
 	def rungif(self, *args):
 		gifid = self.ids['image_gif']
 		
@@ -40,6 +29,8 @@ class StartupGif(BoxLayout, Image):
 		else:
 			gifid.source = 'initiatestart.zip'
 	
+class ScrollableLabel(ScrollView):
+	text = StringProperty('')
 class KivyStart(App):
 	
 	def build(self):

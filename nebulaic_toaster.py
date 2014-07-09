68e2aad314a2922b1b5afb5e7628c8a8dd848382
chromebookbob <chromebookbob@gmail.com>
2014-07-09 18:57:22 +0100

    bugs ironed

--- nebulaic_toaster.py
+++ nebulaic_toaster.py
@@ -1,8 +1,4 @@
 from kivy.app import App
-
-from kivy.uix.label import Label
-from kivy.uix.scrollview import ScrollView
-from kivy.uix.scatter import Scatter
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -11,40 +7,73 @@ from kivy.properties import ListProperty
 from kivy.properties import StringProperty
 from kivy.properties import NumericProperty
 from time import sleep
-
-
 import random
 
-
-
-class ScatterTextWidget(BoxLayout, Image):
+class MainGui(BoxLayout, Image):
 	textboxtext = StringProperty('')
-	def printout(self, thetext):
+	capnum = 113
+	button_pressed = 0
+	def printout(self, thetext, type):
 		tbt = self.ids['textbox1']
-		tbt.text = tbt.text + " " + thetext
+		thetext = " " + thetext + " " 
+		if type == 'add':
+			tbt.text = tbt.text + thetext
+		elif type == 'newline':
+			tbt.text = tbt.text + "\n captain%d@shipA113: %s" % (self.capnum, thetext)
+		else:
+			tbt.text = thetext
+	def start(self, *args):
+		Story.begin()
+			
 	def button1press(self, *args):
-		self.printout("Hello WOrld")
+		button_pressed = 1
+	def button2press(self, *args):
+		button_pressed = 2
+	def button3press(self, *args):
+		button_pressed = 3
+	def button4press(self, *args):
+		button_pressed = 4		
+		
+		
 class StartupGif(BoxLayout):
-
+	
 	def rungif(self, *args):
 		gifid = self.ids['image_gif']
-		
+		button1 = self.ids['initiate_button']
 		if gifid.source =='initiatestart.zip':
-			KivyStart2().run()	
+			button1.size = [0,0]
+			GameName().run()
+			
 		else:
 			gifid.source = 'initiatestart.zip'
-	
-class ScrollableLabel(ScrollView):
-	text = StringProperty('')
+	def donothing(self, *args):
+		pass
 class KivyStart(App):
 	
 	def build(self):
 		return StartupGif()
 		
-class KivyStart2(App):
+class GameName(App):
+	
 	def build(self):
-		return ScatterTextWidget()
+		return MainGui()
+	
+		
 
 if __name__ == "__main__":
 	KivyStart().run()
+	
+	
+class Story():
+	def begin(self, *args):
+		text = """ ******||||| THIS IS AN AUTOMATED MESSAGE |||||******
+		
+			Welcome Captain %d, you have been woken from cryosleep to control this ship.
+		You will receive instructions on a daily basis outlining your course of action. You have a computer terminal, and four keys.
+		These instructions must be followed. Any deviation will be noted, failure to comply will result in your demis. You hold the fate of your race in your hands.
+		Good luck Captain."""
+		MainGui.printout(text, 'asok')
+			
+	
+	
 	
